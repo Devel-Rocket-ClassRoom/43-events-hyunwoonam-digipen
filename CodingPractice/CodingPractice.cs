614d2a3bd21464e775e6326ae31a7473a0a574c9
DESKTOP-NJGSL36\ok623 <hyunwoo.nam@digipen.edu>
2026-03-11 16:39:36 +0900

    작성중

---
+++ CodingPractice/CodingPractice.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Button
+{
+    public event ButtonClickHandler Click;
+
+    public void OnClick()
+    {
+        Click?.Invoke();
+    }
+}
+
+class Player
+{
+    public int _health = 100;
+
+    public event Action<int> DamageTaken;
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        Console.WriteLine($"플레이어 체력: {_health}");
+        DamageTaken?.Invoke(_health);
+    }
+}
+
+class HealthBar
+{
+    public void OnPlayerDamaged(int currHealth)
+    {
+        Console.WriteLine($"[UI] 체력바 업데이트: {currHealth}%");
+    }
+}
+
+class SoundManager
+{
+    public void OnPlayerDamaged(int currentHealth)
+    {
+        Console.WriteLine("[Sound] 피격 효과음 재생");
+    }
+}
+
+class Timer
+{
+    public event Action Tick;
+    private int count;
+    public void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            count++;
+            Console.WriteLine($"타이머: {count}초");
+            Tick?.Invoke();
+        }
+    }
+}
+
+class Logger
+{
+    public void OnTick()
+    {
+        Console.WriteLine("[Logger] 틱 기록됨");
+    }
+}
+
+class Sensor
+{
+    public event Action<string> Alert;
+
+    public void Detect(string message)
+    {
+        Console.WriteLine($"감지: {message}");
+        Alert?.Invoke(message);
+    }
+}
+
+class GameCharacter
+{
+    public event Action OnDeath;
+    public event Action<int> OnDamaged;
+    public event Action<int, string> OnAttack;
+
+    private int _health = 100;
+    private string _name;
+
+    public GameCharacter(string name)
+    {
+        _name = name;
+    }
+    public void Attack(int damage, string targetName)
+    {
+        OnAttack?.Invoke(damage, targetName);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        OnDamaged?.Invoke(_health);
+
+        if (_health <= 0)
+        {
+            OnDeath?.Invoke();
+        }
+
+    }
+}
+
+class PriceChangedEventArgs : EventArgs
+{
+    public decimal OldPrice { get; set; }
+    public decimal NewPrice { get; set; }
+    public decimal ChangePercent { get; set; }
+
+    public PriceChangedEventArgs(decimal oldPrice, decimal newPrice)
+    {
+        OldPrice = oldPrice;
+        NewPrice = newPrice;
+
+        if (oldPrice != 0)
+        {
+            ChangePercent = (newPrice - oldPrice) / oldPrice * 100;
+        }
+    }
+}
+
+class Stock
+{
+    public string _symbol;
+    public decimal _price;
+
+    public event EventHandler<PriceChangedEventArgs> PriceChanged;
+
+    public Stock(string symbol, decimal initialPrice)
+    {
+        _symbol = symbol;
+        _price = initialPrice;
+    }
+
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (_price == value)
+            {
+                return;
+            }
+
+            decimal oldPrice = _price;
+            _price = value;
+
+            OnPriceChanged(new PriceChangedEventArgs(oldPrice, _price));
+        }
+    }
+
+    protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+    {
+        PriceChanged?.Invoke(this, e);
+    }
+
+    public override string ToString()
+    {
+        return $"{_symbol}: {_price:C}";
+    }
+}
+
+class FuelEventArgs : EventArgs
+{
+    public int Fuel { get; }
+    public string Warning { get; }
+    public FuelEventArgs(int fuel, string warning)
+    {
+        Fuel = fuel;
+        Warning = warning;
+    }
+}
+
+class Car
+{
+    private int fuelLevel;
+
+    public event EventHandler<FuelEventArgs> FuelLow;
+    public event Action<int> FuelChanged;
+
+    public Car(int init)
+    {
+        fuelLevel = init;
+    }
+    public int FuelLevel => fuelLevel;
+
+    public void Drive()
+    {
+        if (fuelLevel <= 0)
+        {
+            Console.WriteLine("연료 없음! 운전 불가");
+            return;
+        }
+
+        fuelLevel -= 10;
+        Console.WriteLine($"운전 중... 연료: {fuelLevel}%");
+
+        FuelChanged?.Invoke(fuelLevel);
+
+        if (fuelLevel <= 0)
+        {
+            OnFuelLow(new FuelEventArgs(fuelLevel, "연료가 바닥났습니다!"));
+        }
+        else if (fuelLevel <= 20)
+        {
+            OnFuelLow(new FuelEventArgs(fuelLevel, "연료가 부족합니다!"));
+        }
+    }
+
+    protected virtual void OnFuelLow(FuelEventArgs e)
+    {
+        FuelLow?.Invoke(this, e);
+    }
+}
+
+class Dashboard
+{
+    public void Subscribe(Car car)
+    {
+        car.FuelChanged += OnFuelChanged;
+        car.FuelLow += OnFuelLow;
+    }
+
+    public void Unsubscribe(Car car)
+    {
+        car.FuelChanged -= OnFuelChanged;
+        car.FuelLow -= OnFuelLow;
+    }
+
+    private void OnFuelChanged(int fuelLevel)
+    {
+        string gauge = new string('█', fuelLevel / 10);
+        Console.WriteLine($"[대시보드] 연료 게이지: {gauge}");
+    }
+
+    private void OnFuelLow(object sender, FuelEventArgs e)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[경고!] {e.Warning} (잔량: {e.Fuel}%)");
+        Console.ResetColor();
+    }
+}
+
+class SecurePublisher
+{
+    private EventHandler _event;
+    private readonly object _lock = new object();
+
+    public event EventHandler _Event
+    {
+        add
+        {
+            lock(_lock)
+            {
+                Console.WriteLine($"구독자 추가: {value.Method.Name}");
+                _event += value;
+            }
+        }
+        remove
+        {
+            lock (_lock)
+            {
+                Console.WriteLine($"구독자 제거: {value.Method.Name}");
+                _event -= value;
+            }
+        }
+    }
+
+    public void Raise()
+    {
+        _event?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class GlobalNotifier
+{
+    public static event Action<string> OnGlobalMessage;
+
+    public static void Send(string message)
+    {
+        Console.WriteLine($"[Global] 메시지 전송: {message}");
+        OnGlobalMessage?.Invoke(message);
+    }
+}
+
+class Module1
+{
+    public Module1()
+    {
+        GlobalNotifier.OnGlobalMessage += HandleMessage;
+    }
+
+    private void HandleMessage(string message)
+    {
+        Console.WriteLine($"[Module1] 수신: {message}");
+    }
+}
+
+class Module2
+{
+    public Module2()
+    {
+        GlobalNotifier.OnGlobalMessage += HandleMessage;
+    }
+
+    private void HandleMessage(string message)
+    {
+        Console.WriteLine($"[Module2] 수신: {message}");
+    }
+}
--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,206 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+{
+    Console.WriteLine("1. 대리자 복습");
+
+    Notify notify = SayHello;
+    notify += SayGoodbye;
+
+    notify();
+
+    void SayHello()
+    {
+        Console.WriteLine("안녕하세요!");
+    }
+
+    void SayGoodbye()
+    {
+        Console.WriteLine("안녕히 가세요!");
+    }
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("2. 기본 이벤트 선언");
+    
+    Button button = new Button();
+
+    button.Click += HandleClick;
+    button.Click += HandleClickAgain;
+
+    button.OnClick();
+
+    void HandleClick()
+    {
+        Console.WriteLine("버튼이 클릭되었습니다!");
+    }
+
+    void HandleClickAgain()
+    {
+        Console.WriteLine("클릭 처리 완료");
+    }
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("3. 이벤트 구독");
+
+    Player player = new Player();
+    HealthBar healthBar = new HealthBar();
+    SoundManager soundManager = new SoundManager();
+
+    player.DamageTaken += healthBar.OnPlayerDamaged;
+    player.DamageTaken += soundManager.OnPlayerDamaged;
+
+    player.TakeDamage(30);
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("4. 이벤트 해제");
+
+    Timer timer = new Timer();
+    Logger logger = new Logger();
+
+    timer.Tick += logger.OnTick;
+
+    Console.WriteLine("=== 구독 상태 ===");
+    timer.Start();
+
+    timer.Tick -= logger.OnTick;
+
+    Console.WriteLine("\n=== 구독 해제 후 ===");
+    timer.Start();
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("5. 람다식 이벤트 핸들러");
+    
+    Sensor sensor = new Sensor();
+
+    sensor.Alert += message =>
+    {
+        Console.WriteLine($"[경보] {message}"); 
+    };
+
+    sensor.Alert += message =>
+    {
+        Console.WriteLine($"[로그] {DateTime.Now}: {message}");
+    };
+
+    sensor.Detect("움직임 감지됨");
+    sensor.Detect("온도 상승");
+
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("6. Action 대리자 활용");
+    
+    GameCharacter character = new GameCharacter("용사");
+
+    character.OnDeath += () => Console.WriteLine("캐릭터가 사망했습니다");
+    
+    character.OnDamaged += health => Console.WriteLine($"남은 체력: {health}");
+    
+    character.OnAttack += (damage, target) =>
+    Console.WriteLine($"{target}에게 {damage} 데미지!");
+
+    character.Attack(50, "슬라임");
+    character.TakeDamage(30);
+    character.TakeDamage(80);
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("7. 표준 이벤트 패턴 (EventArgs)");
+
+    Stock stock = new Stock("MSFT", 100.00m);
+
+    stock.PriceChanged += (sender, e) =>
+    {
+        Stock stock = (Stock)sender;
+        Console.WriteLine($"[{stock}]");
+        Console.WriteLine($"  이전 가격: {e.OldPrice:C}");
+        Console.WriteLine($"  현재 가격: {e.NewPrice:C}");
+        Console.WriteLine($"  변동률: {e.ChangePercent:F2}%\n");
+    };
+
+    stock.Price = 110.00m;
+    stock.Price = 105.50m;
+    stock.Price = 120.00m;
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("8. 실전 예제 - 연료 경고 시스템");
+
+    Car car = new Car(50);
+    Dashboard dashboard = new Dashboard();
+
+    dashboard.Subscribe(car);
+
+    for(int i = 0; i < 7;i++)
+    {
+        car.Drive();
+        Console.WriteLine();
+    }
+
+    dashboard.Unsubscribe(car);
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("9. 이벤트 접근자");
+
+    SecurePublisher publisher = new SecurePublisher();
+
+    publisher._Event += Handler1;
+    publisher._Event += Handler2;
+
+    Console.WriteLine("\n이벤트 발생:");
+    publisher.Raise();
+
+    Console.WriteLine();
+    publisher._Event -= Handler1;
+
+    Console.WriteLine("\n이벤트 발생:");
+    publisher.Raise();
+
+    void Handler1(object sender, EventArgs e)
+    {
+        Console.WriteLine("Handler1 실행됨");
+    }
+
+    void Handler2(object sender, EventArgs e)
+    {
+        Console.WriteLine("Handler2 실행됨");
+    }
+}
+
+Console.WriteLine();
+
+{
+    Console.WriteLine("10. static 이벤트");
+
+    Module1 m1 = new Module1();
+    Module2 m2 = new Module2();
+
+    GlobalNotifier.Send("시스템 시작");
+    Console.WriteLine();
+    GlobalNotifier.Send("데이터 로드 완료");
+}
+
+delegate void Notify();
+delegate void ButtonClickHandler();
+
